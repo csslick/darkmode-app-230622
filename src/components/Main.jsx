90c61d4510e0ce6868048fdc7e52308190a33fc0
csslick <digitone@naver.com>
2023-06-22 15:30:54 +0900

    public image test

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,10 @@ function Main(props) {
   return (
     <main>
       <h1>main</h1>
+      <ul>
+        <li><img src="images/일론.jpg" width="500" alt="일론" />다크모두쥐!</li>
+        <li><img src="images/000101.png" alt="000101" /></li>
+      </ul>
     </main>
   )
 }
